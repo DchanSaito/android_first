b08362d80756596231c87b32bbff85a477cc6a6e
DchanSaito <dchan1230develop@gmail.com>
2017-06-21 18:00:25 +0900

    記事タイトル一覧を出せるようにした

---
+++ app/src/main/java/site/dchan/www/hello_kotlin/ArticleListAdapter.kt
@@ -0,0 +1,27 @@
+package site.dchan.www.hello_kotlin
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import site.dchan.www.hello_kotlin.model.Article
+import site.dchan.www.hello_kotlin.view.ArticleView
+import java.text.FieldPosition
+
+
+class ArticleListAdapter(private val context: Context) : BaseAdapter() {
+    var articles: List<Article> = emptyList()
+
+    override fun getCount(): Int = articles.size
+
+    override fun getItem(position: Int): Any? = articles[position]
+
+    override fun getItemId(position: Int): Long = 0
+
+    override fun getView(position: Int,
+                           convertView: View?,
+                           parent: ViewGroup?): View =
+            ((convertView as? ArticleView) ?: ArticleView(context)).apply {
+                setArticle((articles[position]))
+            }
+}
--- app/src/main/java/site/dchan/www/hello_kotlin/MainActivity.kt
+++ app/src/main/java/site/dchan/www/hello_kotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package site.dchan.www.hello_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import site.dchan.www.hello_kotlin.model.Article
 import site.dchan.www.hello_kotlin.model.User
 import site.dchan.www.hello_kotlin.view.ArticleView
@@ -10,17 +11,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        // ArticleViewオブジェ作成
-        val articleView = ArticleView(applicationContext)
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(dummyArticle("Kotlin", "hogeo"), dummyArticle("Java", "foo"))
 
-        // ArticleObjを生成して、ArticleViewにセット
-        articleView.setArticle(Article(id = "123",
-                title = "Kotlin",
-                url = "http://www.example.com/articles/123",
-                user = User(id = "456", name = "hogeo", profileImageUrl = "")))
-
-        // Layout
-        setContentView(articleView)
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlinlang.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
